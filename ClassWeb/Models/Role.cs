5ea4478cb1484147d93f8e2aa37d7a11765867cb
simkkish <simkkish@isu.edu>
2019-02-12 12:39:54 -0700

    #2 completed

--- ClassWeb/Models/Role.cs
+++ ClassWeb/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,47 @@ namespace ClassWeb.Models
 {
     public class Role
     {
+        private int _ID;
+        private int _Title;
+        private int _Description;
+        private DateTime _DateCreated;
+        private DateTime _DateModified;
+        private DateTime _DateDeleted;
+        [Key]
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+        public int Title
+        {
+            get { return _Title; }
+            set { _Title = value; }
+        }
+
+        public int Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+        public DateTime DateCreated
+        {
+            get { return _DateCreated; }
+            set { _DateCreated = value; }
+        }
+
+
+        public DateTime DateModified
+        {
+            get { return _DateModified; }
+            set { _DateModified = value; }
+        }
+
+        public DateTime DateDeleted
+        {
+            get { return _DateDeleted; }
+            set { _DateDeleted = value; }
+        }
+
     }
 }
