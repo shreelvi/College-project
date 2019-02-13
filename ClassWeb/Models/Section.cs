3194e00eb79a9de83ce725893adab223fdbc0a57
msm07 <madamoha@isu.edu>
2019-02-12 18:55:42 -0700

    Test Link

---
+++ ClassWeb/Models/Section.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassWeb.Models
+{
+    public class Section
+    {
+        //hh
+    }
+}
