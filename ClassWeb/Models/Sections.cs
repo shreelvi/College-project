50405cd38ce6753fdf613270836f2c36cc298d95
msm07 <madamoha@isu.edu>
2019-02-13 12:39:52 -0700

    Model of Sections

---
+++ ClassWeb/Models/Sections.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassWeb.Models
+{
+    public class Sections
+    {
+        private int _ID;
+        private int _SectionNumber;
+        private int _ClassID;
+        private int _UserID;
+
+        [Key]
+        public int ID
+        {
+            get
+            {
+                return _ID;
+            }
+
+            set
+            {
+                _ID = value;
+            }
+        }
+
+        public int SectionNumber
+        {
+            get
+            { return _SectionNumber;
+            }
+
+            set
+            { _SectionNumber = value;
+            }
+        }
+
+
+        public int ClassID
+        {
+            get
+            {
+                return _ClassID;
+            }
+
+            set
+            {
+                _ClassID = value;
+            }
+        }
+
+
+        public int UserID
+        {
+            get
+            {
+                return _UserID;
+            }
+
+            set
+            {
+                _UserID = value;
+            }
+        }
+
+
+
+
+
+
+    }
+}
