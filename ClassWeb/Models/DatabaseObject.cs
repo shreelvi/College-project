94eeddf9437fa1a0ef56ec99e3bd4af96b6814b2
simkkish <simkkish@isu.edu>
2019-02-19 20:58:15 -0700

    Inheritance and code cleanup

---
+++ ClassWeb/Models/DatabaseObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassWeb.Models
+{
+    public class DatabaseObject
+    {
+        #region Private Variable
+        private int _ID;
+        #endregion
+        #region Public Class
+        [Key]
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+        #endregion
+    }
+}
