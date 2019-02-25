9c0521fe0246be1328c98b74028086a89776e8a4
simkkish <simkkish@isu.edu>
2019-02-24 22:47:13 -0700

    Upload And Download Completed

--- ClassWeb/Startup.cs
+++ ClassWeb/Startup.cs
@@ -63,7 +63,7 @@ namespace ClassWeb
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Upload}/{action=Index}/{id?}");
             });
         }
     }
