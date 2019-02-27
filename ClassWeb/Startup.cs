c47481d7f0efd3c3d13b4d5b1bc1018fc8722721
simkkish <simkkish@isu.edu>
2019-02-27 03:49:37 -0700

    Environment variable disabled

--- ClassWeb/Startup.cs
+++ ClassWeb/Startup.cs
@@ -45,15 +45,15 @@ namespace ClassWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
