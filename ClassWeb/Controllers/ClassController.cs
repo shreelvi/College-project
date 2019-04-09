e4f447ff082d1851549ed6e8224a68ff483b5e95
Ganesh <sapkgane@isu.edu>
2019-04-08 20:02:24 -0600

    updating class features

--- ClassWeb/Controllers/ClassController.cs
+++ ClassWeb/Controllers/ClassController.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 using ClassWeb.Model;
 using ClassWeb.Models;
 using ClassWeb.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClassWeb.Controllers
 {
-    public class ClassController : Controller
+    public class ClassController : BaseController
     {
         ///<summary>
         /// created by Ganesh
@@ -32,23 +33,85 @@ namespace ClassWeb.Controllers
             _emailService = emailService;
         }
         #endregion
-        // GET: Class
-        public ActionResult Index()
+
+        #region sendEmail
+        /// <summary>
+        /// Code By: Elvis
+        /// Modified by Ganesh
+        /// Date Created: 03/29/2019
+        /// Reference: https://steemit.com/utopian-io/@babelek/how-to-send-email-using-asp-net-core-2-0
+        /// https://stackoverflow.com/questions/35881641/how-can-i-send-a-confirmation-email-in-asp-net-mvc
+        /// Used the code in these References to add feature to send confirmation email to user when registering
+        /// Not complete yet. Some issue to fix when sending an email.
+        /// </summary>
+
+        [AllowAnonymous]
+        public ActionResult SendEmail()
         {
-            ViewBag.Class = DAL.GetClass();
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("account/SendEmail")]
+        public async Task<IActionResult> SendEmailAsync(string email, string subject, string message)
+        {
+            await _emailService.SendEmail(email, subject, message);
+            TempData["Message"] = "Email Successfully Sent!!";
+            return RedirectToAction("Dashboard", "Class");
+        }
+        [AllowAnonymous]
+        #endregion
+
+        #region Class
+        /// <summary>
+        /// by:Ganesh
+        /// Reference: Elvis Code on role controller& PeerEval 
+        /// </summary>
+        /// <returns></returns>
+        // GET: Class
+        public IActionResult Index()
+        {
+            User LoggedIn = CurrentUser;
+
+            //Gets error message to display from Create method 
+            var a = TempData["ClassAdd"];
+            if (a != null)
+                ViewData["RoleAdd"] = a;
+            //Gets error message to display from Delete method 
+            var d = TempData["ClassDelete"];
+            if (d != null)
+                ViewData["ClassDelete"] = d;
+            #region Testing Code
+            List<Class> Class = new List<Class>();
+            Class = DAL.GetClass();
+            return View(Class);
+            #endregion
         }
 
         // GET: Class/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
-            ViewData["Message"] = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
-            return View();
+            Class roleObj = DAL.GetClass(id);
+            if (roleObj == null)
+            {
+                return NotFound();
+            }
+
+            return View(roleObj);
         }
 
         // GET: Class/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
+            User LoggedIn = CurrentUser;
+
+            //Checks if the user is logged in
+            if (LoggedIn.FirstName == "Anonymous")
+            {
+                TempData["LoginError"] = "Please login to view the page.";
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
 
@@ -60,47 +123,77 @@ namespace ClassWeb.Controllers
         /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public IActionResult Create ([Bind("Title,IsAvailable,DateStart,DateEnd,SectionID")]Class newClass)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
+                if (ModelState.IsValid)
+                {
+                   int i = DAL.AddClass(newClass);           
+                   return RedirectToAction(nameof(Index));
+                }
                 return View();
-            }
         }
 
         // GET: Class/Edit/5
-        public ActionResult Edit(int id)
+        public IActionResult Edit(int? id)
         {
-            return View();
+            int? cid = HttpContext.Session.GetInt32("ClassID");
+            if (cid != null)
+            {
+                id = cid;
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var classes = DAL.ClassGetByID(id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+            return View(classes);
         }
 
         // POST: Class/Edit/5
-        /// <summary>
-        /// to edit the class details for the user
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="collection"></param>
-        /// <returns></returns>
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+        public async Task <IActionResult> Edit(int? id,[Bind("Title, IsAvailable, DateStart, DateEnd,SectionID,ID")]Class classes)
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+        {
+            if (id != classes.ID)
             {
-                return View();
+                return NotFound();
             }
+            int? cid = HttpContext.Session.GetInt32("ClassID");
+            if (id == null && cid != null)
+            {
+                id = cid;
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (id == cid)
+                    {
+                        int a = DAL.UpdateClass(classes);
+                        if (a > 0)
+                        {
+                            HttpContext.Session.SetInt32("ID", classes.ID);
+                            TempData["Message"] = "Class Succesfully Updated!!";
+                        }
+                    }
+                    else
+                    {
+                        TempData["Message"] = "Trick!!";
+                    }
+                    return RedirectToAction("Dashboard", "Class");
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return RedirectToAction("Dashboard", "Class");
         }
 
         // GET: Class/Delete/5
@@ -109,26 +202,33 @@ namespace ClassWeb.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Class classes = DAL.ClassGetByID(id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            return View(classes);
         }
 
         // POST: Class/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete(int id)
         {
-            try
+            int test = DAL.DeleteClassByID(id);
+            if (test > 0)
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
+                ViewBag.Message = "Class Succesfully Deleted!!";
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction(nameof(Index));
         }
         /// <summary>
         /// logs out the user and clear their session information. 
@@ -146,4 +246,5 @@ namespace ClassWeb.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+    #endregion
 }
