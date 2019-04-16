cf93aac6b7308e7d11b39df298c314eefe51ea2a
Ganesh <sapkgane@isu.edu>
2019-04-15 21:59:22 -0600

    updating CourseSsemester, semester

--- ClassWeb/Controllers/CoursesSemestersController.cs
+++ ClassWeb/Controllers/CoursesSemestersController.cs
@@ -2,92 +2,169 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClassWeb.Models;
 
 namespace ClassWeb.Controllers
 {
-    public class CoursesSemestersController : Controller
+    /// <summary>
+    /// Created on: 04/09/2019
+    /// Created by: Elvis
+    /// CRUD controller for CourseSemester class
+    /// </summary>
+    public class CourseSemestersController : Controller
     {
-        // GET: CoursesSemesters
-        public ActionResult Index()
+        private readonly ClassWebContext _context;
+
+        public CourseSemestersController(ClassWebContext context)
         {
+            _context = context;
+        }
+
+        // GET: CourseSemesters
+        public async Task<IActionResult> Index()
+        {
+            var classWebContext = _context.CourseSemester.Include(c => c.Course).Include(c => c.User);
+            return View(await classWebContext.ToListAsync());
+        }
+
+        // GET: CourseSemesters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseSemester = await _context.CourseSemester
+                .Include(c => c.Course)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (courseSemester == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseSemester);
+        }
+
+        // GET: CourseSemesters/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseID"] = new SelectList(_context.Set<Course>(), "ID", "ID");
+            ViewData["UserID"] = new SelectList(_context.Set<User>(), "ID", "ID");
             return View();
         }
 
-        // GET: CoursesSemesters/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: CoursesSemesters/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CoursesSemesters/Create
+        // POST: CourseSemesters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create([Bind("CourseID,SemesterID,YearID,SectionID,UserID,ID")] CourseSemester courseSemester)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
+                _context.Add(courseSemester);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewData["CourseID"] = new SelectList(_context.Set<Course>(), "ID", "ID", courseSemester.CourseID);
+            ViewData["UserID"] = new SelectList(_context.Set<User>(), "ID", "ID", courseSemester.UserID);
+            return View(courseSemester);
         }
 
-        // GET: CoursesSemesters/Edit/5
-        public ActionResult Edit(int id)
+        // GET: CourseSemesters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseSemester = await _context.CourseSemester.FindAsync(id);
+            if (courseSemester == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseID"] = new SelectList(_context.Set<Course>(), "ID", "ID", courseSemester.CourseID);
+            ViewData["UserID"] = new SelectList(_context.Set<User>(), "ID", "ID", courseSemester.UserID);
+            return View(courseSemester);
         }
 
-        // POST: CoursesSemesters/Edit/5
+        // POST: CourseSemesters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("CourseID,SemesterID,YearID,SectionID,UserID,ID")] CourseSemester courseSemester)
         {
-            try
+            if (id != courseSemester.ID)
             {
-                // TODO: Add update logic here
+                return NotFound();
+            }
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(courseSemester);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseSemesterExists(courseSemester.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewData["CourseID"] = new SelectList(_context.Set<Course>(), "ID", "ID", courseSemester.CourseID);
+            ViewData["UserID"] = new SelectList(_context.Set<User>(), "ID", "ID", courseSemester.UserID);
+            return View(courseSemester);
         }
 
-        // GET: CoursesSemesters/Delete/5
-        public ActionResult Delete(int id)
+        // GET: CourseSemesters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseSemester = await _context.CourseSemester
+                .Include(c => c.Course)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (courseSemester == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseSemester);
         }
 
-        // POST: CoursesSemesters/Delete/5
-        [HttpPost]
+        // POST: CourseSemesters/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
-            {
-                // TODO: Add delete logic here
+            var courseSemester = await _context.CourseSemester.FindAsync(id);
+            _context.CourseSemester.Remove(courseSemester);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+        private bool CourseSemesterExists(int id)
+        {
+            return _context.CourseSemester.Any(e => e.ID == id);
         }
     }
 }
--- ClassWeb/Models/Year.cs
+++ ClassWeb/Models/Year.cs
@@ -5,24 +5,33 @@ using System.Threading.Tasks;
 
 namespace ClassWeb.Models
 {
-    public class Semester : DatabaseNamedRecord
+    public class Year : DatabaseRecord
     {
 
         #region Constructors
-        public Semester()
+        public Year()
         {
         }
-        internal Semester(MySql.Data.MySqlClient.MySqlDataReader dr)
+        internal Year(MySql.Data.MySqlClient.MySqlDataReader dr)
         {
             Fill(dr);
         }
 
         #endregion
 
+        private int _Year;
+
+
+
+        public int Year1
+        {
+            get { return _Year; }
+            set { _Year = value; }
+        }
 
         #region Database String
-        internal const string db_ID = "SemesterID";
-        internal const string db_Name = "SemesterName";
+        internal const string db_ID = "YearID";
+        internal const string db_Year = "Year";
         #endregion
 
         #region Public Functions
@@ -51,7 +60,7 @@ namespace ClassWeb.Models
         public override void Fill(MySql.Data.MySqlClient.MySqlDataReader dr)
         {
             _ID = dr.GetInt32(db_ID);
-            _Name = dr.GetString(db_Name);
+            _Year = dr.GetInt32(db_Year);
         }
 
         public override string ToString()
