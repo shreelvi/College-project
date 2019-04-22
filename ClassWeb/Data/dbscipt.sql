f9ceae18f14d32141161082420911cf8ddb9bf28
Ganesh <sapkgane@isu.edu>
2019-04-22 08:12:24 -0600

    updating dbscript and making changes

--- ClassWeb/Data/dbscipt.sql
+++ ClassWeb/Data/dbscipt.sql
@@ -190,3 +190,21 @@ CREATE TABLE `GroupsUsers` (
     FOREIGN KEY (`UserID`)
     REFERENCES `Users` (`UserID`)
  );
+
+ -- -----------------Course------------------------------
+-- ======================================================
+
+-- Author: Mohan
+-- Description:	Create course table in the database
+-- ======================================================
+
+CREATE TABLE `course` (
+  `CourseID` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `Subject` VARCHAR(50) NOT NULL,
+  `CourseNumber` INT(11)  NOT NULL,
+  `CourseTitle` VARCHAR(50) NOT NULL);
+  
+INSERT INTO course(Subject,  CourseNumber,CourseTitle) 
+	VALUES ( 'INFO', '4482', 'System Development Implementation Method');
+
+
