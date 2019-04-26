f335e3d52d59aa0a155ffec6bff3a1d05bbfbbbf
Sakshi Khetan <khetsaks@isu.edu>
2019-04-26 12:15:25 -0600

    startup page

--- ClassWeb/Startup.cs
+++ ClassWeb/Startup.cs
@@ -22,7 +22,7 @@ namespace ClassWeb
         public bool EnableDirectoryBrowsing { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
